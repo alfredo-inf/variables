a51c53728cdd8052f55c922cf63003eeda390204
Your Name <alfredoquezada0@gmail.com>
2024-01-25 00:13:16 -0400

    first commit

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+
+        String tipo = "Computadora";
+        final double Tasa_dolar = 59.50;
+        boolean disponible = false;
+        final String mensaje_bienvenida = "Bienvenido a este módulo ";
+        int resultado = 4*9;
+        double monto = 775.49;
+        double montoConITBIS =  monto * 1.18;
+        System.out.println(montoConITBIS);
+    }
+}
